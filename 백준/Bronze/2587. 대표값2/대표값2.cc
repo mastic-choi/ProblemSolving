d886445952e35754b16a6e3815e45920951e1693
CHOI JUNSOO <jschoi04@gmail.com>
2026-03-24 22:52:52 +0900

    Update 대표값2.cc
    algorith의 sort를 사용하면 시간 초과가 발생한다. 라고 생각했는데 그냥 list index를 잘못선언했다

--- 백준/Bronze/2587. 대표값2/대표값2.cc
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void insertion_sort(int list[4]);
+//void insertion_sort(int list[4]);
 
 int main(void){
     ios_base::sync_with_stdio(false);
@@ -17,10 +17,10 @@ int main(void){
     }
     ava = ava/5;
     cout << ava << "\n";
-    // sort(a, a+5);
-    //algorith의 sort를 사용하면 시간 초과가 발생한다.
+    sort(a, a+5);
+    //algorith의 sort를 사용하면 시간 초과가 발생한다. 라고 생각했는데 그냥 list index를 잘못선언했다/
     //삽입 정렬로 해보자
-    insertion_sort(a);
+    // insertion_sort(a);
     
     cout << a[2] << "\n";
 
@@ -28,14 +28,14 @@ int main(void){
 }
 
 
-void insertion_sort(int list[4]){
-    int key, j;
-    for(int i = 1; i < 5; i++){
-        key = list[i];
+// void insertion_sort(int list[4]){
+//     int key, j;
+//     for(int i = 1; i < 5; i++){
+//         key = list[i];
 
-        for(j = (i -1); j >= 0 && list[j]>key; j--){
-            list[j+1] = list[j];
-        }
-        list[j+1] = key;
-    }
-}
+//         for(j = (i -1); j >= 0 && list[j]>key; j--){
+//             list[j+1] = list[j];
+//         }
+//         list[j+1] = key;
+//     }
+// }
